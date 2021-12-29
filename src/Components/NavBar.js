27eb587172fd5a89072c6ea156c34478ef39cd5f
OL4JUWON <juwon.lawal@tm30.net>
2021-12-29 20:05:17 +0100

    nav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,16 +5,16 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <NavBar.Wrapper>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light ">
        <NavLink className="navbar-brand" to="/" > <img src={Logo} alt='' /></NavLink>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button className="navbar-toggler" type="icon" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
       <li className="nav-item active">
-        <NavLink className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
+        <NavLink className="nav-link" to="/">Home </NavLink>
       </li>
          
       <li className="nav-item dropdown">
@@ -32,12 +32,14 @@ const NavBar = () => {
         </NavLink>
         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
           <NavLink className="dropdown-item" to="/secureprints">Security Printing</NavLink>
+          <NavLink className="dropdown-item" to="/CommercialPrints">Commercial Print</NavLink>
           <NavLink className="dropdown-item" to="/envelope">Envelope Production</NavLink>
-          <NavLink className="dropdown-item" to="/envelope">Variable Data Printing</NavLink>
+          <NavLink className="dropdown-item" to="/variabledata">Variable Data Printing</NavLink>
+          <NavLink className="dropdown-item" to="/Thermal">Thermal/POS roll Production</NavLink>
            </div>
       </li> 
       <li className="nav-item dropdown">
-        <NavLink className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <NavLink className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Blog/Media
         </NavLink>
         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -57,168 +59,43 @@ const NavBar = () => {
 </nav>
 
 
-      {/* <div className="logo">
-                <img src={Logo} alt='' />
-            </div>
-    <div className="Menu">
-        <ul >
-           <li><NavLink to={"/"}>Home</NavLink> </li>
-        <li >About us<i className="fa fa-caret-down"></i>
-        <ul>
-                <li><NavLink to="/About">The Company</NavLink></li>
-      <li><NavLink to="/team">The Team</NavLink></li></ul>
-        </li>
-            <li >Our Solutions<i className="fa fa-caret-down"></i>
-            <ul>
-            <li><NavLink to="/SecurePrints">Security Prints</NavLink></li>
-            <li><NavLink to="/CommercialPrints">Commercial Print</NavLink></li>
-                <li><NavLink to="/variabledata">Variable Data Print</NavLink></li>
-                <li><NavLink to="/BrandProtection">Brand Protection</NavLink></li>
-                <li><NavLink to="/Envelope">Envelope Production</NavLink></li>
-                <li><NavLink to="/Thermal">Thermal/POS roll Production</NavLink></li>
-      <li><NavLink to="#">Link 3</NavLink></li></ul></li>
-            <li><NavLink to={"/Blog"}>Blog/Media<i className="fa fa-caret-down"></i></NavLink>
-            <ul>
-                <li><NavLink to="/News">News/Articles</NavLink></li>
-      <li><NavLink to="/Gallery">Gallery</NavLink></li></ul></li>
-            <li><NavLink to={"/faq"}>FAQ</NavLink></li>
-            <li><NavLink to={"/contact"}>Contact</NavLink> </li> 
-
-        </ul>
-    </div> */}
+      
     </NavBar.Wrapper>
   )
 }
 
 NavBar.Wrapper = styled.div`
-.navbar-nav{
-// padding-right: 150px;
-}
-.navbar-collapse{
-  width: 100%;
-  text-align: right;
-}
+// .navbar-nav{
+// // padding-right: 150px;
+// }
+
 
 
 // height: 100px;
 @media only screen and (max-width: 600px) {
- nav{
-   background-color: #05172B;
- }
+
+ .navbar-collapse{
+  width: 50%;
+  text-align: right;
+  .dropdown-menu{
+    text-align: right;
+    // width: 50%;
+    border: 0;
+    justify-content: right;
+    direction: rtl;
+  }
+}
 .navbar-brand{
   img{
     width: 200px;
     height: 100px;
   }
-}
-.nav-item{
-border-bottom: 1px solid #1A66FE;
-}
-}
+} 
 
-.navbar-light .navbar-toggler-icon {
-  // background-image: url("data:image/svg+xml;..");
-  color: blue;
 }
-// this is a white icon with 50% opacity
-.navbar-dark .navbar-toggler-icon {
- color:blue;
-}
-// // position:absolute;
-// // z-index: 100;
 // background:url(${BACKGROUND}) rgba(5, 23, 43, 0.6) ; 
-// // positio¿n: absolute;
-// background-blend-mode: multiply;
-// // padding: 10px;
-// width: 100%;
-// box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
-// display: flex;
-// justify-content: space-between;
-// align-items: center;
-// .logo{
-//     display: flex;
-//     margin-left: 100px;
-// }
-// a {
-//     color: #011166;
-//     text-decoration: none;
-// }
-// i{
-//     margin: 0 5px;
-// }
-// .Menu{
-//     width: 50vw;
-   
-//     ul {
-//         background: white;
-//         list-style: none;
-//         margin: 0;
-//         padding-left: 0;
-//       }
-      
-//       li {
-//         color: #011166;
-//        // background: white;
-//         display: block;
-//         float: left;
-//         padding: 1rem;
-//         position: relative;
-//         text-decoration: none;
-//         transition-duration: 0.5s;
-//       }
-        
-//       li a {
-//         color: #011166;
-//       }
-//       li a:hover {
-//           color: white;
-//       }
-      
-//       li:hover {
-//         background: #011166;
-//         color: white;
-//         cursor: pointer;
-//       }
-      
-//       ul li ul {
-//         background: orange;
-//         visibility: hidden;
-//         opacity: 0;
-//         min-width: 5rem;
-//         position: absolute;
-//         transition: all 0.5s ease;
-//         margin-top: 1rem;
-//         left: 0;
-//         display: none;
-//       }
-      
-//       ul li:hover > ul,
-//       ul li ul:hover {
-//         visibility: visible;
-//         opacity: 1;
-//         display: block;
-//       }
-      
-//       ul li ul li {
-//         clear: both;
-//         width: 100%;
-//       }
-// }
-// @media only screen and (max-width: 768px) {
-  
-//   background:url(${BACKGROUND}) rgba(5, 23, 43, 0.6) ; 
-//   // positio¿n: absolute;
-//   background-blend-mode: multiply;
-// opacity: 0.1;
-//   .logo{
-//     width: 200px;
-//     padding:2px;
-//     margin : 0;
-//   }
-//   .Menu{
-//     display: none;
-//   }
-// } 
+
+
 `
 
 export default NavBar;
